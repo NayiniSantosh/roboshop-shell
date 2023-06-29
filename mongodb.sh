5f82bac169ccf48d874fd27438a466ef9fe4f1b6
nayin <santoshnainwar9505@gmail.com>
2023-06-29 19:02:18 +0530

    RoboShop nginx Script.

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,12 @@
 echo -e "\e[34mCopy MongoDB Repo file\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
 echo -e "\e[34mInstalling MongoDB Server\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 
 
 #modify the config file(localhost(127.0.0.1))
 echo -e "\e[34mStart MongoDB Server\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
