ff3007496c634001468f9bd0ba683f7d9467133d
nayin <santoshnainwar9505@gmail.com>
2023-06-30 20:31:11 +0530

    Shipping Script

--- shipping.sh
+++ shipping.sh
@@ -1,34 +1,37 @@
 echo -e "\e[34mInstall Maven \e[0m"
-yum install maven -y
+yum install maven -y &>>/tmp/roboshop.log
 
 echo -e "\e[34m Add Application User \e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[34m Create Application Directory\e[0m"
-rm -rf /app
-mkdir /app
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[34m Download Application Content\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
 
 
 echo -e "\e[34m Extract Application Content \e[0m"
-unzip /tmp/shipping.zip
+unzip /tmp/shipping.zip &>>/tmp/roboshop.log
 
 echo -e "\e[34m Download Maven Dependencies \e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+mvn clean package &>>/tmp/roboshop.log
+mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 
 echo -e "\e[34m Install MySQL Client \e[0m"
-yum install mysql -y
+yum install mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[34m Load Schema \e[0m"
-mysql -h mysql-dev.sandevops.site -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql-dev.sandevops.site -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
+
+echo -e "\e[34m Setup SystemD File \e[0m"
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
 
 echo -e "\e[34m Start Shipping Service \e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable shipping &>>/tmp/roboshop.log
+systemctl restart shipping &>>/tmp/roboshop.log
 
 
 
