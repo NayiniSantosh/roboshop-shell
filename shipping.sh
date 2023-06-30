dd12ffc1621ba6273c51bbaefd68c27ec9b385ef
nayin <santoshnainwar9505@gmail.com>
2023-06-30 21:42:21 +0530

    Shipping Script

--- shipping.sh
+++ shipping.sh
@@ -10,11 +10,10 @@ mkdir /app
 
 echo -e "\e[34m Download Application Content\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
-
+cd /app
 
 echo -e "\e[34m Extract Application Content \e[0m"
-cd /app
-unzip /tmp/shipping.zip &>>/tmp/roboshop.log
+unzip /tmp/shipping.zip
 
 echo -e "\e[34m Download Maven Dependencies \e[0m"
 mvn clean package &>>/tmp/roboshop.log
@@ -24,15 +23,28 @@ echo -e "\e[34m Install MySQL Client \e[0m"
 yum install mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[34m Load Schema \e[0m"
-mysql -h mysql-dev.sandevops.site -uroot -pRoboShop@1 </app/schema/shipping.sql &>>/tmp/roboshop.log
+mysql -h mysql-dev.sandevops.site -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
+
 
 echo -e "\e[34m Setup SystemD File \e[0m"
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service  &>>/tmp/roboshop.log
 
-echo -e "\e[34m Start Shipping Service \e[0m"
+
+echo -e "\e[34m Start Shipping Service\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable shipping &>>/tmp/roboshop.log
 systemctl restart shipping &>>/tmp/roboshop.log
+
+
+
+
+
+
+
+
+
+
+
 
 
 
