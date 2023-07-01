88c590b6251ec972d5ccde607d1aba65d594424f
nayin <santoshnainwar9505@gmail.com>
2023-07-01 14:49:42 +0530

    RabbitMQ Script

---
+++ rabbitmq.sh
@@ -0,0 +1,17 @@
+echo -e "\e[34m Configure Erlang Repos \e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
+
+echo -e "\e[34m  Configure RabbitMQ Repos \e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
+
+echo -e "\e[34m Install RabbitMQ Server \e[0m"
+yum install rabbitmq-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[34m Start RabbitMQ Service \e[0m"
+systemctl enable rabbitmq-server &>>/tmp/roboshop.log
+systemctl restart rabbitmq-server &>>/tmp/roboshop.log
+
+
+echo -e "\e[34m Add RabbitMQ Application User \e[0m"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
